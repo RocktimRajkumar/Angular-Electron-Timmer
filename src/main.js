8dea15ac13ef58fd5b61a091519a445530f25e4f
Rocktim <rajkumar.rocktim@gmail.com>
2019-09-10 08:57:36 +0530

    src > main.js : file added and electron window created

---
+++ src/main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let win;
+
+function createWindow() {
+    // Create the browser window
+    win = new BrowserWindow({
+        width: 600,
+        height: 600,
+        backgroundColor: '#ffffff',
+        icon: `file://${__dirname}/dist/assets/logo.png`
+    });
+
+    win.loadURL(path.join(__dirname, '../dist/angular-electron-timmer/index.html'));
+
+    // Event when the window is closed
+    win.on('closed', function () {
+        win = null
+    });
+
+}
+
+// Create window on electron initialization
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+    // On macOS specific close process
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
